1aed8d9379aea5ab474e259dd8e9b778140a5440
zhyvr <46420014+zhyvr@users.noreply.github.com>
2022-12-06 15:13:09 +0300

    Vanilla JS

---
+++ kaesar.js
@@ -0,0 +1,34 @@
+const alphabet = "ێیەھۆونمڵلگکقڤفغعشسژزڕردخحچجتپبائ";
+let output ="";
+
+function encrypt(text, key) {
+  for (let char of text) {
+    if (alphabet.includes(char)) {
+      let position = alphabet.indexOf(char);
+      let newPosition = (position + key) % 33;
+      let newChar = alphabet[newPosition];
+      output += newChar;
+    } else {
+      output += char;
+    }
+  }
+  return output;
+}
+
+function decrypt(text, key) {
+  for (let char of text) {
+    if (alphabet.includes(char)) {
+      let position = alphabet.indexOf(char);
+      let newPosition = (position - key) % 33;
+      let originalChar = alphabet[newPosition];
+      output += originalChar;
+    } else {
+      output += char;
+    }
+  }
+  return output;
+}
+const text = "بردنەوە"; //شتێک بنووسە
+const key = 3; //کلیل
+console.log(encrypt(text, key)); // بۆ ئینکریپت
+//console.log(decrypt(text, key)); //بۆ دیکریپت
